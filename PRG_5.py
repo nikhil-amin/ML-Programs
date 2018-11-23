9d946f1158163eb86fbb87a956f879795313dd46
Nikhil Amin <aminnikhil073@gmail.com>
2018-11-23 22:41:50 +0530

    Update PRG_5.py

--- PRG_5.py
+++ PRG_5.py
@@ -1,3 +1,7 @@
+# (QUESTION: 10)
+# Implement the non-parametric Locally Weighted Regression algorithm in order to
+# fit data points. Select appropriate data set for your experiment and draw graphs. 
+
 import csv
 import random
 import math
